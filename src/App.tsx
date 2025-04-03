06ae92c22052b51cf8cb55fbcfd91d56ebb7cf62
argoQM <argo08qm@outlook.com>
2025-04-03 12:00:48 -0300

    Comienzo de la personalización de la web
    Se comenzó con la incorporación de imágenes del grupo y la personalización de la web mediante la selección de temas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import React from "react";
-
-function seleccionarActivo(activo: string) {
-  const colores: Element | null = document.querySelector(".activo");
-  if (colores != null) {
-    colores.classList.remove("k4os");
-    colores.classList.remove("tau");
-    colores.classList.remove("mechi");
-    colores.classList.remove("ine");
-    colores.classList.remove("lily");
-    colores.classList.add(activo);
-  }
-}
-function seleccionarTemaActivo(activo: string) {
-  const colores = document.querySelectorAll(".temaActivo");
-  for (let i: number = 0; i < colores.length; i++) {
-    // console.log(colores[i]);
-
-    if (colores != null) {
-      colores[i].classList.remove("caos");
-      colores[i].classList.remove("extra");
-      colores[i].classList.remove("menteEnBlanco");
-      colores[i].classList.remove("rage");
-      colores[i].classList.remove("uffBaby");
-      colores[i].classList.add(activo);
-    }
-  }
-}
+import React, { useState } from "react";
+import datos from "./datos.json";
 
 function App() {
+  const info = datos;
+
+  // imagen principal
+
+  const [imgPrincipalSrc, setImgPrincipalSrc] = useState(
+    info.k4os.img.k4osLogo.src
+  );
+  const [imgPrincipalTitle, setImgPrincipalTitle] = useState(
+    info.k4os.img.k4osLogo.title
+  );
+  const [imgPrincipalAlt, setImgPrincipalAlt] = useState(
+    info.k4os.img.k4osLogo.alt
+  );
+
+  function seleccionarActivo(activo: string) {
+    const colores: Element | null = document.querySelector(".activo");
+    if (colores != null) {
+      colores.classList.remove("k4os");
+      colores.classList.remove("tau");
+      colores.classList.remove("mechi");
+      colores.classList.remove("ine");
+      colores.classList.remove("lily");
+      colores.classList.add(activo);
+    }
+  }
+  function seleccionarTemaActivo(activo: string) {
+    const colores = document.querySelectorAll(".temaActivo");
+    for (let i: number = 0; i < colores.length; i++) {
+      if (colores != null) {
+        colores[i].classList.remove("caos");
+        colores[i].classList.remove("extra");
+        colores[i].classList.remove("menteEnBlanco");
+        colores[i].classList.remove("rage");
+        colores[i].classList.remove("uffBaby");
+        colores[i].classList.add(activo);
+      }
+    }
+  }
   return (
     <div className="h-screen activo bg-fondo">
       <div className=" h-1/5 flex justify-between">
@@ -36,6 +48,9 @@ function App() {
           onClick={() => {
             seleccionarActivo("k4os");
             seleccionarTemaActivo("k4os");
+            setImgPrincipalSrc(info.k4os.img.k4osLogo.src);
+            setImgPrincipalTitle(info.k4os.img.k4osLogo.title);
+            setImgPrincipalAlt(info.k4os.img.k4osLogo.alt);
           }}
         >
           K4os
@@ -77,6 +92,9 @@ function App() {
         <div
           onClick={() => {
             seleccionarTemaActivo("caos");
+            setImgPrincipalSrc(info.k4os.img.caos.src);
+            setImgPrincipalTitle(info.k4os.img.caos.title);
+            setImgPrincipalAlt(info.k4os.img.caos.alt);
           }}
           className="flex justify-center items-center bg-caos text-white  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
         >
@@ -85,6 +103,9 @@ function App() {
         <div
           onClick={() => {
             seleccionarTemaActivo("extra");
+            setImgPrincipalSrc(info.k4os.img.extra.src);
+            setImgPrincipalTitle(info.k4os.img.extra.title);
+            setImgPrincipalAlt(info.k4os.img.extra.alt);
           }}
           className="flex justify-center items-center bg-extra text-white  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
         >
@@ -93,6 +114,9 @@ function App() {
         <div
           onClick={() => {
             seleccionarTemaActivo("menteEnBlanco");
+            setImgPrincipalSrc(info.k4os.img.menteEnBlanco.src);
+            setImgPrincipalTitle(info.k4os.img.menteEnBlanco.title);
+            setImgPrincipalAlt(info.k4os.img.menteEnBlanco.alt);
           }}
           className="flex justify-center items-center bg-menteEnBlanco text-black  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
         >
@@ -101,14 +125,20 @@ function App() {
         <div
           onClick={() => {
             seleccionarTemaActivo("rage");
+            setImgPrincipalSrc(info.k4os.img.rage.src);
+            setImgPrincipalTitle(info.k4os.img.rage.title);
+            setImgPrincipalAlt(info.k4os.img.rage.alt);
           }}
-          className="flex justify-center items-center bg-Rage text-yellow-500  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
+          className="flex justify-center items-center bg-rage text-yellow-500  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
         >
           Rage
         </div>
         <div
           onClick={() => {
             seleccionarTemaActivo("uffBaby");
+            setImgPrincipalSrc(info.k4os.img.uffBaby.src);
+            setImgPrincipalTitle(info.k4os.img.uffBaby.title);
+            setImgPrincipalAlt(info.k4os.img.uffBaby.alt);
           }}
           className="flex justify-center items-center bg-uffBaby text-pink-500  text-2xl text-center rounded-3xl w-30 h-20 hover:cursor-pointer"
         >
@@ -117,7 +147,12 @@ function App() {
       </div>
       <div className=" h-2/5 flex ">
         <div className="w-1/3 bg-fondoTema flex justify-center aspect-auto items-center temaActivo text-white">
-          imagen
+          <img
+            className="h-full aspect-auto"
+            src={imgPrincipalSrc}
+            alt={imgPrincipalAlt}
+            title={imgPrincipalTitle}
+          />
         </div>
         <div className="w-2/3 bg-fondoTema flex justify-center aspect-video items-center rounded-3xl temaActivo text-white">
           video
@@ -157,3 +192,56 @@ function App() {
 }
 
 export default App;
+
+// "album1": {
+//     "caos": {
+//       "video": {
+//         "url": "",
+//         "title": ""
+//       },
+//       "tau": {},
+//       "mechi": {},
+//       "ine": {},
+//       "lily": {}
+//     },
+//     "extra": {
+//       "video": {
+//         "url": "",
+//         "title": ""
+//       },
+//       "tau": {},
+//       "mechi": {},
+//       "ine": {},
+//       "lily": {}
+//     },
+//     "menteEnBlanco": {
+//       "video": {
+//         "url": "",
+//         "title": ""
+//       },
+//       "tau": {},
+//       "mechi": {},
+//       "ine": {},
+//       "lily": {}
+//     },
+//     "rage": {
+//       "video": {
+//         "url": "",
+//         "title": ""
+//       },
+//       "tau": {},
+//       "mechi": {},
+//       "ine": {},
+//       "lily": {}
+//     },
+//     "uffBaby": {
+//       "video": {
+//         "url": "",
+//         "title": ""
+//       },
+//       "tau": {},
+//       "mechi": {},
+//       "ine": {},
+//       "lily": {}
+//     }
+//   }
